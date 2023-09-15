37781407777782e6b65a53f565e8b804942072ca
meganm672 <meganm672@gmail.com>
2023-09-15 13:55:14 -0400

    completed the sign up component tab

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -8,14 +8,24 @@ export default function SignUpForm(){
     async function handleSubmit(e){
         //prevent the normal behavior
         e.preventDefault();
-        console.log("Hello!")
+       try{
+            const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup",{
+                method: "POST",
+                data: JSON.stringify({username,password})
+            })
+            const data = await response.json();
+            console.log(data);
+       }catch(error){
+            setError(error.message)
+       }
     }
     return(
         <div>
             <h2>Sign Up</h2>
+            {error && <p>{error}</p>}
             <form onSubmit={handleSubmit}>
                 <label>
-                  Username:  
+                  Username:{" "}
                   <input 
                     value={username} 
                     onChange={(e) => {setUsername(e.target.value)}}
@@ -23,7 +33,7 @@ export default function SignUpForm(){
                 </label>
                 <br/>
                 <label>
-                 Password:  
+                 Password:{" "}
                  <input 
                     value={password} 
                     onChange={(e) => {setPassword(e.target.value)}}
